fe26905f3d5134ec55247792cbe502fce3e70d4f
nhamer <norman.hamer@gmail.com>
2024-06-19 08:59:20 -0700

    Handle Win10 (which doesn't render events) better, don't bother to include merged perfdata since we don't care

--- BthA2dpCodec.cmd
+++ BthA2dpCodec.cmd
@@ -7,19 +7,26 @@ echo %0 must be run at an elevated command prompt
 goto :eof
 :elevated
 
-REM curl -o BluetoothStack.wprp https://raw.githubusercontent.com/microsoft/busiotools/master/bluetooth/tracing/BluetoothStack.wprp
-REM Microsoft.Windows.Bluetooth.BthA2dp, 8776ad1e-5022-4451-a566-f47e708b9075
+rem curl -o BluetoothStack.wprp https://raw.githubusercontent.com/microsoft/busiotools/master/bluetooth/tracing/BluetoothStack.wprp
+rem Microsoft.Windows.Bluetooth.BthA2dp, 8776ad1e-5022-4451-a566-f47e708b9075
 
 set TRACEPATH=%TEMP%
 set TRACENAME=BthA2DPTrace
 set TRACEETLNAME=%TRACEPATH%\%TRACENAME%.etl
 set TRACETXTNAME=%TRACEPATH%\%TRACENAME%.txt
-netsh trace start report=disabled provider={8776ad1e-5022-4451-a566-f47e708b9075} overwrite=yes traceFile="%TRACEETLNAME%" 
+netsh trace start report=disabled perfMerge=no provider={8776ad1e-5022-4451-a566-f47e708b9075} overwrite=yes traceFile="%TRACEETLNAME%" 
 echo Start your audio stream
 pause
 netsh trace stop
 netsh trace convert input="%TRACEETLNAME%" output="%TRACETXTNAME%" dump=TXT overwrite=yes
+find "A2dpStreaming" %TRACETXTNAME% > NUL
+if %ERRORLEVEL% EQU 0 goto rendered
+rem Win10 apparently doesn't have the manifest to render, so just dump all events from BthA2dp
+type %TRACETXTNAME% | findstr Microsoft.Windows.Bluetooth.BthA2dp
+goto dumpdone
+:rendered
 type %TRACETXTNAME% | findstr \"A2dpStreaming\"
+:dumpdone
 
 del %TRACEETLNAME%
 del %TRACETXTNAME%
